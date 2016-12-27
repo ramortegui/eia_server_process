7fe0d80a5ead89ec80420431890ca1b30d06abf6
Ruben Amortegui <ruben.amortegui@gmail.com>
2016-12-27 12:58:43 -0700

    Add cast functions for request that doesn't need a response ( asynchronous )

--- lib/server_process.ex
+++ lib/server_process.ex
@@ -8,21 +8,31 @@ defmodule ServerProcess do
 
   defp loop( callback_module, current_state ) do
     receive do
-      {request, caller} -> 
+      {:call, request, caller} -> 
         {response , new_state} = callback_module.handle_call(
           request,
           current_state
         )
         send(caller, {:response, response })
         loop(callback_module, new_state)
+      {:cast, request } ->
+        new_state = callback_module.handle_cast(
+          request,
+          current_state
+        )
+        loop(callback_module, new_state)
     end
   end
 
   def call(server_pid, request) do
-    send(server_pid, {request, self})
+    send(server_pid, {:call, request, self})
     receive do
       {:response, response } -> response
     end
+  end
+
+  def cast(server_pid, request) do
+    send(server_pid, {:cast, request})
   end
 end
 
@@ -37,15 +47,15 @@ defmodule KeyValueStore do
   end
 
   def put(pid, key, value) do
-    ServerProcess.call(pid, {:put, key, value})
+    ServerProcess.cast(pid, {:put, key, value})
   end
 
   def get(pid,key) do
     ServerProcess.call(pid, {:get, key})
   end
 
-  def handle_call({:put, key, value}, state) do
-    {:ok, Map.put(state, key, value)}
+  def handle_cast({:put, key, value}, state) do
+    Map.put(state, key, value)
   end
 
   def handle_call({:get, key}, state) do
